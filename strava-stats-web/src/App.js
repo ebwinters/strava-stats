8cce052912f14deb05d0751f01dacfe34f360fa5
ebwinters@comcast.net <ebwinters@comcast.net>
2023-07-18 13:28:20 -0700

    web

---
+++ strava-stats-web/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Stats } from './Stats';
+
+function App() {
+  return (
+    <div className="App">
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
---
+++ strava-stats-web/src/Stats.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { AnimatedCounter } from  'react-animated-counter';
+
+const baseUrl = 'https://strava-stats.azurewebsites.net/api/strava-stats';
+
+export const fetchStats = async () => {
+    
+}
+
+export function Stats() {
+    const [ride, setRide] = React.useState(0);
+    const [run, setRun] = React.useState(0);
+
+    useEffect(() => {
+        // declare the async data fetching function
+        const fetchData = async () => {
+          const data = await fetch(baseUrl);
+          const json = await data.json();
+          setRide(json.Ride);
+          setRun(json.Run)
+        }
+        fetchData()
+          .catch(console.error);;
+    }, [])
+    
+    return (
+        <div>
+            <h1 className="h1">Ride</h1>
+            <AnimatedCounter
+                includeDecimals={true}
+                value={ride}
+                color='black'
+                fontSize='40px'
+            />
+            <h1>Run</h1>
+            <AnimatedCounter
+                includeDecimals={true}
+                value={run}
+                color='black'
+                fontSize='40px'
+            />
+        </div>
+    );
+}
